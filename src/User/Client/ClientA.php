2e5129ff19d36a2a152e7ba802327301394c3d1b
xiao <626223818@qq.com>
2019-02-11 05:15:58 -0500

    init

---
+++ src/User/Client/ClientA.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiao
+ * Date: 2/11/19
+ * Time: 4:49 AM
+ */
+
+namespace User\Client;
+
+
+class ClientA
+{
+    /**
+     * No explanation
+     *
+     * @author Zhiqiang Guo
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index()
+    {
+        echo "this is clientA";
+    }
+}
---
+++ src/User/Client/ClientB.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiao
+ * Date: 2/11/19
+ * Time: 4:49 AM
+ */
+
+namespace User\Client;
+
+
+class ClientB
+{
+    /**
+     * No explanation
+     *
+     * @author Zhiqiang Guo
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index()
+    {
+        echo "this is clientB";
+    }
+}
